6beab53620cdb32f0cb514cc76be0c8d7fc35c93
Renkai <gaelookair@gmail.com>
2013-05-31 09:34:51 +0800

    Update swap_fifo.c

--- code/lab3/kern/mm/swap_fifo.c
+++ code/lab3/kern/mm/swap_fifo.c
@@ -48,6 +48,7 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
     list_entry_t *entry=&(page->pra_page_link);
  
     assert(entry != NULL && head != NULL);
+     list_add(head,entry);
     //record the page access situlation
     /*LAB3 EXERCISE 2: YOUR CODE*/ 
     //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
@@ -67,6 +68,12 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
      /*LAB3 EXERCISE 2: YOUR CODE*/ 
      //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
      //(2)  set the addr of addr of this page to ptr_page
+	 list_entry_t *vict=head->prev;
+	 assert(head!=vict);
+	 struct Page *p=le2page(vict,pra_page_link);
+	 list_del(vict);
+	 assert(p!=NULL);
+	 *ptr_page=p;
      return 0;
 }
 
